d43397e2cc502025bfd16632fd25a5f33446049f
chri6tina <christinashumpert17@icloud.com>
2025-09-05 11:21:16 -0400

    Revert to static Ahrefs analytics script implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,16 +73,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var ahrefs_analytics_script = document.createElement('script');
-              ahrefs_analytics_script.async = true;
-              ahrefs_analytics_script.src = 'https://analytics.ahrefs.com/analytics.js';
-              ahrefs_analytics_script.setAttribute('data-key', 'SMECEsnHAn9OBCpmwyJBcA');
-              document.getElementsByTagName('head')[0].appendChild(ahrefs_analytics_script);
-            `,
-          }}
+        <script 
+          src="https://analytics.ahrefs.com/analytics.js" 
+          data-key="SMECEsnHAn9OBCpmwyJBcA" 
+          async
         />
       </head>
       <body
